0e26b2c1d40a8bb70a3b57442bc5c279697226d8
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-05-13 01:39:37 -0700

    2013-11-19 8:04 Commit
    Added assets folder to hold screenshots, some new data files,
    blink_clench.m, AC_EEG_data.mat, and updated main.m, loaddata.m

--- MATLAB/loaddata.m
+++ MATLAB/loaddata.m
@@ -1,64 +1,51 @@
-%% prep
-clear;
-close all;
-clc;
+function loaddata(input, mode)
 %% get the input file
-M = csvread('10clenches.csv');
+display('Importing CSV file.')
+M = csvread(input,1);
 EEG_data = M(:,3:16);
 clear M;
-display('Input successfully imported')
-%% prepare the time index
-for i=1:length(EEG_data(:,1))
+display('CSV file successfully imported.')
+%% remove the DC offset, code provided by Emotiv
+IIR_TC = 256;
+[rows, columns]= size(EEG_data); 
+AC_EEG_data = zeros(rows, columns); 
+back = EEG_data(1,:);
+for r = 2 : rows
+    back = (back*( IIR_TC- 1 ) + EEG_data( r,:)) / IIR_TC; 
+    AC_EEG_data( r,:) = EEG_data( r,:)- back;
+end
+%% if mode == 2, then we want to run the filter i made previously on the data too
+if mode==2
+    %% prepare the DC removing filter for use
+    display('Preparing Anthonys Filter.')
+    d = fdesign.highpass('Fst,Fp,Ast,Ap',0.1,0.16,60,1, 128);
+    Hd = design(d,'butter');
+    fvtool(Hd)
+    %% normalize the data 
+%     simple normalizing
+%     for i=1:14 
+%         to_normalize = AC_EEG_data(:,i);
+%         normalized = ( to_normalize - (.5*(max(to_normalize)+min(to_normalize))) ) / (.5*( max(to_normalize) - min(to_normalize) )); %normalize between -1 and 1
+%         AC_EEG_data(:,i) = normalized;
+%     end
+%     clear to_normalize;
+%     clear normalized;
+%     display('Data succesfully normalized')
+    %% run the filter on the normalized data
+    display('Running Anthonys Filter.')
+    for i=1:14
+        to_filter = AC_EEG_data(:,i);
+        filtered = filter(Hd,to_filter);
+        AC_EEG_data(:,i) = filtered;
+    end
+end
+%% add time back in
+for i=1:length(AC_EEG_data(:,1))
 	time(i) = i;
 end
 time = time';
-%% prepare the DC removing filter for use
-% d = fdesign.highpass('Fst,Fp,Ast,Ap',0.1,0.16,60,1, 128);
-
-N    = 5;     % Order
-F3dB = 0.16;  % 3-dB Frequency
-Fs   = 128;   % Sampling Frequency
-
-d = fdesign.highpass('n,f3db', N, F3dB, Fs);
-
-Hd = design(d,'butter');
-fvtool(Hd)
-%clear d;
-%% normalize the data so that the filter works properly
-% simple normalizing
-for i=1:14 
-    to_normalize = EEG_data(:,i);
-    normalized = ( to_normalize - (.5*(max(to_normalize)+min(to_normalize))) ) / (.5*( max(to_normalize) - min(to_normalize) )); %normalize between -1 and 1
-    EEG_data(:,i) = normalized;
+AC_EEG_data(:,15) = time;
+%% output the resulting file
+save('AC_EEG_data.mat', 'AC_EEG_data')
+display('AC_EEG_data.mat sucessfully created.')
 end
-clear to_normalize;
-clear normalized;
-display('Data succesfully normalized')
-%% run the filter on the normalized data
-for i=1:14 
-    to_filter = EEG_data(:,i);
-    filtered = filter(Hd,to_filter);
-    EEG_data_copy(:,i) = filtered;
-end
-clear to_filter;
-% clear filtered;
-% clear Hd;
-%% extract the processed datasets
-AF3 = EEG_data(:,1);
-F7 = EEG_data(:,2);
-F3 = EEG_data(:,3);
-FC5 = EEG_data(:,4);
-T7 = EEG_data(:,5);
-P7 = EEG_data(:,6);
-O1 = EEG_data(:,7);
-O2 = EEG_data(:,8);
-P8 = EEG_data(:,9);
-T8 = EEG_data(:,10);
-FC6 = EEG_data(:,11);
-F4 = EEG_data(:,12);
-F8 = EEG_data(:,13);
-AF4 = EEG_data(:,14);
-display('Done.')
-%% clean up
-% clear EEG_data;
-clear i;
--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,44 +1,33 @@
 close all;
 clc;
 %% SETUP
-% corresponds to step 0
-% -getting the data into the program
-loaddata;
+% loaddata('data/anthony-1minclenches-19.11.13.17.25.47.csv','1')
+% loaddata('data/anthony-1minlniks-19.11.13.17.23.35.csv','1')
+loaddata('data/anthony-1minmixed-19.11.13.19.12.03.csv','1')
+load('AC_EEG_data.mat');
+
+%% extract the processed datasets
+AF3 = AC_EEG_data(:,1);
+F7 = AC_EEG_data(:,2);
+F3 = AC_EEG_data(:,3);
+FC5 = AC_EEG_data(:,4);
+T7 = AC_EEG_data(:,5);
+P7 = AC_EEG_data(:,6);
+O1 = AC_EEG_data(:,7);
+O2 = AC_EEG_data(:,8);
+P8 = AC_EEG_data(:,9);
+T8 = AC_EEG_data(:,10);
+FC6 = AC_EEG_data(:,11);
+F4 = AC_EEG_data(:,12);
+F8 = AC_EEG_data(:,13);
+AF4 = AC_EEG_data(:,14);
+time = AC_EEG_data(:,15);
+display('Done.')
+
 verbosity = 1;
-% Fs = 128+1;
-% look_ahead_percentage = .75;
-thresholding_percentage = .5;
-% min_samples_to_next_peak = int32(look_ahead_percentage*Fs);
-% min_samples_to_next_peak = 1;
 
-%% Find the peaks of the thresholded signal and plot them
-% corresponds to step 1
+ch1 = F7;	%blink and clench
+ch2 = T7;	%clench only
 
-
-to_threshold = F8;
-[pks,locs]=findpeaks(to_threshold,'MINPEAKDISTANCE',50,'MINPEAKHEIGHT',(thresholding_percentage*1*max(to_threshold)));
-% adjust this section some more
-
-if verbosity == 1
-	figure;
-	newFiltered=zeros(length(to_threshold),1);
-	plot(to_threshold)
-	hold all;
-	plot(locs, pks, 'o')
-	title('Clench Peaks', 'FontWeight','bold')
-end
-
-%% Segment the data and plot it
-% corresponds to step 2
-edges_locs = generate_edges_from_locs(locs);
-segmented = segment_by_edge(time, filtered, edges_locs);
-
-if verbosity == 1
-    figure;
-    plot(time,filtered)
-    for j=1:length(segmented)
-        hold all;
-        plot(segmented{j}(:,1),segmented{j}(:,2))
-    end
-    title('Segmented Clenches', 'FontWeight','bold')
-end
+result = blink_clench(ch1,ch2,time,1);
+display(result');
