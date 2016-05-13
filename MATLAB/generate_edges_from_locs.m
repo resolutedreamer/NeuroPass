74a72b99e9c47bc6947b21d75e16f413a4dbb090
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-05-13 01:54:09 -0700

    2013-12-04 6:30 PM Commit

--- MATLAB/generate_edges_from_locs.m
+++ MATLAB/generate_edges_from_locs.m
@@ -4,29 +4,12 @@ function [edges_locs] = generate_edges_from_locs(peaks)
 % as the midpoint between two peaks.
 
 i=1;
-
 st=1;
-
 sort(peaks);
 
 while i<length(peaks)
-    %nd=peaks(i);
-	%edge = int32( (nd+st)/2 );
-    
-    %if (i==1)
-    %    edge = i;
-    %    edge_locs(i) = edge;
-    %elseif (i==length(peaks)-1)
-    %    edge = i;
-    %    edge_locs(i) = i;
-    %else
-        edge = int32( (peaks(i)+peaks(i+1)) /2 );
-        edges_locs(i) = edge;
-    %end
-    
+	edge = int32( (peaks(i)+peaks(i+1)) /2 );
+	edges_locs(i) = edge;
 	st = edge +1;   
-    i =i+1; 
+    i = i+1; 
 end
-
-%length(edge)
-%length(peaks)
