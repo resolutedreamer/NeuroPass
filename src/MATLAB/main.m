b9a0ffcd6b47f8245b5572ca601a605898e9bfda
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2017-03-02 12:18:16 -0800

    revised main.m
    - removed duplicate code
    - improved variable naming for clarity

--- src/MATLAB/main.m
+++ src/MATLAB/main.m
@@ -1,38 +1,41 @@
 close all;
 clear;
 clc;
-%% CHOOSE USAGE MODE
+%% STEP 1) CHOOSE USAGE MODE
 display('Welcome to NeuroPass Version 1.0');
 display('What would you like to do:');
 display('a) Set a new password.');
 display('b) Authenticate against the existing password.');
 str = input('Type either "a" or "b".\n','s');
 
-%% LOAD THE FILE
+%% STEP 2) LOAD THE FILE
 [filename,pathname] = uigetfile('.csv','Please enter the path to your file.');
 fullpath = strcat(pathname,filename);
 
-%% PROCESS THE FILE
-result = result_generator(fullpath);
+%% STEP 3) PROCESS THE FILE
+event_sequence = process_raw_data(fullpath);
 
-%% RUN EXPECTED USAGE MODE
+%% GET FILE HASH
+% SHA-1 broken
+Opt.Method = 'SHA-256';
+% DataHash https://www.mathworks.com/matlabcentral/fileexchange/31272-datahash?requestedDomain=www.mathworks.com
+password = DataHash(event_sequence, Opt);
+
+%% STEP 4) RUN EXPECTED USAGE MODE
 if str == 'a'
-    Opt.Method = 'SHA-1';
-    password = DataHash(result, Opt);
+    % Save the hash of the password, i.e. the golden keys. Never lose this.
+    save('password.mat', password)
     display('You have saved a new password:');
     display(password);
-    save('password.mat',password)
+    display('You should delete the input file immediately to preserve security.');
     clear;
 elseif str == 'b'
     % load the stored password
-    load('password.mat')
-    % check the hash of the current file
-    Opt.Method = 'SHA-1';
-    password = DataHash(result, Opt);
+    storedpassword = load('password.mat')    
     % verify the match
     if (password == storedpassword)
     end
-    %display the result
+    % display the result
     if (match)
         display('Your file matches the current password!');
     else
