3b05ebe246f1684f0a212bc4ff743dc58dda110d
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2017-03-11 22:40:21 -0800

    Fix process_raw_data
    Now correctly has 3 return values and loads the csv into memory directly
    without a the use of an intermediate file

--- src/MATLAB/process_raw_data.m
+++ src/MATLAB/process_raw_data.m
@@ -1,36 +1,31 @@
-function [ch1, ch2] = process_raw_data(input_path)
-% process_raw_data Loads raw data from Emotiv into MATLAB
-%   Detailed explanation goes here
+function [ch1, ch2, time] = process_raw_data(input_path)
+% process_raw_data Loads raw data from Emotiv CSV files into MATLAB
 
-% convert_csv_to_mat('data/anthony-1minclenches-19.11.13.17.25.47.csv')
-% convert_csv_to_mat('data/anthony-1minlniks-19.11.13.17.23.35.csv')
-% convert_csv_to_mat('data/anthony-1minmixed-19.11.13.19.12.03.csv')
+%% STEP 1) Convert the data
+AC_EEG_data = convert_csv_to_mat(input_path);
 
-%% STEP 1) Load the data
-convert_csv_to_mat(input_path);
-load('AC_EEG_data.mat');
+%% Alt STEP 1) Load the data from saved file
+%AC_EEG_data = load('AC_EEG_data.mat');
 
 %% STEP 2) Seperate each channel
-AF3 = AC_EEG_data(:,1);
-F7 = AC_EEG_data(:,2);
-F3 = AC_EEG_data(:,3);
-FC5 = AC_EEG_data(:,4);
-T7 = AC_EEG_data(:,5);
-P7 = AC_EEG_data(:,6);
-O1 = AC_EEG_data(:,7);
-O2 = AC_EEG_data(:,8);
-P8 = AC_EEG_data(:,9);
-T8 = AC_EEG_data(:,10);
-FC6 = AC_EEG_data(:,11);
-F4 = AC_EEG_data(:,12);
-F8 = AC_EEG_data(:,13);
-AF4 = AC_EEG_data(:,14);
+AF3  = AC_EEG_data(:,1);
+F7   = AC_EEG_data(:,2);
+F3   = AC_EEG_data(:,3);
+FC5  = AC_EEG_data(:,4);
+T7   = AC_EEG_data(:,5);
+P7   = AC_EEG_data(:,6);
+O1   = AC_EEG_data(:,7);
+O2   = AC_EEG_data(:,8);
+P8   = AC_EEG_data(:,9);
+T8   = AC_EEG_data(:,10);
+FC6  = AC_EEG_data(:,11);
+F4   = AC_EEG_data(:,12);
+F8   = AC_EEG_data(:,13);
+AF4  = AC_EEG_data(:,14);
 time = AC_EEG_data(:,15);
 display('Done.')
 
 %% STEP 3) Select channels of interest
 ch1 = F7;	%blink and clench
 ch2 = T7;	%clench only
-
 end
-
